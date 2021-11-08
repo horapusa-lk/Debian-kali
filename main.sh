7003fa2be2f3df48c614a55c4ed4090d1395c072
Bhathiya Gamage <73831309+pakayaaaa@users.noreply.github.com>
2021-11-08 18:01:18 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,15 @@
+#!/bin/bash
 
-#update
-pkg update -y
-pkg upgrade -y
+RD='\e[1;31m'
+GR='\e[1;32m'
+YL='\e[1;33m'
+BL='\e[1;34m'
+PP='\e[1;35m'
+LBL='\e[1;36m'
+GY='\e[1;30m'
+WH='\e[1;37m'
+
+clear
 
 #install req
 apt install wget -y
@@ -10,6 +18,26 @@ pkg install ruby -y
 pkg install figlet -y
 gem install lolcat -y
 pkg install proot-distro -y
+
+clear
+figlet Kali Linux | lolcat
+echo
+echo -e $GR "Tool From Hora Pusa"
+
+echo
+echo -e $RD "You may use this tool at your own risk. The developers at Zyntax™ do not hold any responsibility"
+sleep 2
+echo
+echo -e $GR "Press Enter to continue.."
+read
+clear
+echo -e $YL "[#] Creating the platform for the rooting process"
+
+
+#update
+pkg update -y
+pkg upgrade -y
+
 
 #install Debain
 proot-distro install ubuntu
